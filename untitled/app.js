c122c92536fc60a288aceea98ba2614dac0b0084
Dennis <dengsauve@yahoo.com>
2018-01-08 11:18:15 -0800

    Completed 1-8-18 Lecture

---
+++ untitled/app.js
@@ -0,0 +1,31 @@
+// This is a single line comment
+
+/*
+This is a multi-line comment
+ */
+
+// data types
+
+/*
+undefined
+null
+boolean
+string
+number
+object
+
+symbol // New in ECMA6
+*/
+
+// These are all used to build objects. They are Primitive Data types.
+
+// = is called an assignment operator
+
+// ++i is prefix notation, i++ is postfix notation
+
+var dog = "Golden Retriever";
+
+var cat, fish;
+
+cat = "Mr. Simon";
+fish = "lunch";
